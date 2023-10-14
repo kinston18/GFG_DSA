59937d05a358723d92996c3c9184ffd6a84fc1df
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-10-14 14:19:22 +0530

    Create Kth smallest element.cpp

---
+++ Medium/Kth smallest element.cpp
@@ -0,0 +1,28 @@
+/*
+
+Input:
+N = 6
+arr[] = 7 10 4 3 20 15
+K = 3
+Output : 7
+Explanation :
+3rd smallest element in the given 
+array is 7.
+
+*/
+
+
+//User function template for C++
+
+class Solution{
+    public:
+    // arr : given array
+    // l : starting index of the array i.e 0
+    // r : ending index of the array i.e size-1
+    // k : find kth smallest element and return using this function
+    int kthSmallest(int arr[], int l, int r, int k) {
+        //code here
+        sort(arr,arr+r+1);
+        return arr[k-1];
+    }
+};
