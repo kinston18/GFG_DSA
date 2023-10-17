8b4c992cee3863ab3e3023db9977c7a376ce0770
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-10-17 23:57:09 +0530

    Create C++ Function overloading.cpp

---
+++ Easy/C++ Function overloading.cpp
@@ -0,0 +1,41 @@
+
+
+//User function Template for C++
+
+/*
+
+  Input:
+
+3
+
+1 2
+
+2 1 2
+
+3 1 2 3
+
+Output:
+
+8
+6.29038
+6
+
+  */
+
+void volume(int s)
+{
+    // Add your code here.
+    cout<<s*s*s<<endl;
+}
+
+void volume(int r, int h)
+{
+    // Add your code here.
+    cout<<3.14159*r*r*h<<endl;
+}
+
+void volume(int l, int b, int h)
+{
+    // Add your code here.
+    cout<<l*b*h<<endl;
+}
