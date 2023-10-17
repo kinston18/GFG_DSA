344cb65f7f49ba8c0aa6750daecf7d1c7f4ef85b
Prajjwal Goswami <77458269+kinston18@users.noreply.github.com>
2023-10-17 23:37:44 +0530

    Create Pattern 2.cpp

---
+++ Easy/Pattern 2.cpp
@@ -0,0 +1,31 @@
+/*
+Example 1:
+
+Input: 5
+
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+ 
+
+
+  */
+
+
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<"* ";
+                
+            }
+            cout<<endl;
+        }
+    }
+};
